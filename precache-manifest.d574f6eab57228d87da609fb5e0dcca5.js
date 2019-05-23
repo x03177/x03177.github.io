482bc8439a9cebde1b95186605c9b984a7336f7a
x03177 <x03177@gmail.com>
2019-05-23 14:18:43 +0800

    try github pages

---
+++ precache-manifest.d574f6eab57228d87da609fb5e0dcca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe1de7548002379bad1e906fbb01314",
+    "url": "../index.html"
+  },
+  {
+    "revision": "1266afb77fd86124c36d",
+    "url": "../static/css/main.919faba8.chunk.css"
+  },
+  {
+    "revision": "93487ae462c4434f71f0",
+    "url": "../static/js/2.668fe80b.chunk.js"
+  },
+  {
+    "revision": "1266afb77fd86124c36d",
+    "url": "../static/js/main.31d6c3fd.chunk.js"
+  },
+  {
+    "revision": "7bb77e5ff9e2fe82860b",
+    "url": "../static/js/runtime~main.3a34a6a7.js"
+  }
+]);
